adc099a1e64fef25a1964f6587f08c090da2c524
Azure Pipeline <azure-pipeline@coso.com>
2023-09-28 21:56:48 +0200

    add trackevent for state / trigger to 5 min

--- MonitorPiOrchestrator/index.ts
+++ MonitorPiOrchestrator/index.ts
@@ -4,10 +4,16 @@ const orchestrator = df.orchestrator(function* (context) {
     let devices: any[] = yield context.df.callActivity("GetDeviceTwinsActivity");
     const parallelTasks = [];
 
+    const appInsights = require('applicationinsights')
+    appInsights.setup();
+    const appInsightsClient = appInsights.defaultClient;
+
     for (let i of devices) {
         let deviceStatus = yield context.df.callActivity("GetPiStatusActivity", i.deviceId);
         deviceStatus.mzr = i.mzr;
         deviceStatus.verificationCode = i.verificationCode;
+        deviceStatus.state = i.state;
+        appInsightsClient.trackEvent({ name: "Monitor Pi Status", properties: { deviceId: deviceStatus.deviceId, status: deviceStatus.running ? "online" : "offline", location: deviceStatus.mzr ? deviceStatus.mzr : "not set", verificationCode: deviceStatus.verificationCode ? deviceStatus.verificationCode : "not set", state: deviceStatus.state ? deviceStatus.state : "inactive" } });
         parallelTasks.push(context.df.callActivity("UpdateDeviceStatus", deviceStatus));
     }
 
