b1862d7f265275f70df7fded9b771f34d6d5ffd6
Azure Pipeline <azure-pipeline@coso.com>
2023-09-28 22:23:53 +0200

    update device state in get twins activity

--- GetDeviceTwinsActivity/index.ts
+++ GetDeviceTwinsActivity/index.ts
@@ -9,7 +9,8 @@ const activityFunction: AzureFunction = async function (context: Context): Promi
         let twin = (await registry.getTwin(d.deviceId)).responseBody
         if (!twin.properties.desired.mzr) twin.properties.desired.mzr = "not set"
         if (!twin.properties.desired.verificationCode) twin.properties.desired.verificationCode = "not set"
-        twins.push({ deviceId: twin.deviceId, mzr: twin.properties.desired.mzr, verificationCode: twin.properties.desired.verificationCode });
+        if (!twin.properties.desired.state) twin.properties.desired.state = "not set"
+        twins.push({ deviceId: twin.deviceId, mzr: twin.properties.desired.mzr, verificationCode: twin.properties.desired.verificationCode, state: twin.properties.desired.state });
     }
     return twins;
 };
